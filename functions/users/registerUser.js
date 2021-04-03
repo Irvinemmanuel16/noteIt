7b74e9c961d9cb80232028bef5218b849817a14d
Irvin Meneses <developer@irvinemmanuel.xyz>
2021-04-02 21:46:59 -0500

    nuevo intento de resolucion

--- functions/users/registerUser.js
+++ functions/users/registerUser.js
@@ -8,8 +8,7 @@ const db_name = encodeURIComponent(process.env.DB_NAME);
 const db_password = encodeURIComponent(process.env.DB_PASSWORD);
 const db_user = process.env.DB_USER;
 
-// const URI = `mongodb+srv://${db_user}:${db_password}@cluster0.w5lai.mongodb.net/${db_name}?retryWrites=true&w=majority`;
-const URI = `mongodb+srv://admin_1:ea7WNKSxtF1gzZtq@cluster0.w5lai.mongodb.net/noteIt?retryWrites=true&w=majority`;
+const URI = `mongodb+srv://${db_user}:${db_password}@cluster0.w5lai.mongodb.net/${db_name}?retryWrites=true&w=majority`;
 
 const UserSchema = new mongoose.Schema({
   name: {
