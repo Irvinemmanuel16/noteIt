1b960a85cee6d675afb9d892b03ad1d3e1de3249
Irvin Meneses <developer@irvinemmanuel.xyz>
2021-04-04 22:22:18 -0500

    Se añadio la funcion loginUser

--- functions/users/loginUser.js
+++ functions/users/loginUser.js
@@ -1,37 +1,20 @@
-const mongoose = require('mongoose');
+const { MongoClient } = require('mongodb');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const Validator = require('validator');
 const isEmpty = require('is-empty');
 
-let conn = null;
 const db_name = encodeURIComponent(process.env.DB_NAME);
 const db_password = encodeURIComponent(process.env.DB_PASSWORD);
 const db_user = process.env.DB_USER;
 
 const URI = `mongodb+srv://${db_user}:${db_password}@cluster0.w5lai.mongodb.net/${db_name}?retryWrites=true&w=majority`;
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
+let cachedDb = null;
 
-}, {
-  timestamps: true
-});
 
-UserSchema.methods.comparePasswords = async function (password) {
-  let isMatch = await bcrypt.compare(password, this.password);
+async function comparePasswords(password, savedPassword) {
+  let isMatch = await bcrypt.compare(password, savedPassword);
   return isMatch;
 };
 
@@ -46,52 +29,59 @@ function validateLogin({ email, password }) {
   if (Validator.isEmpty(password)) errors.push({ msg: 'Password field is required', id: 13});
 
   if (!isEmpty(errors)) throw errors;
+
+  return { email, password }
+}
+
+async function connectToDatabase() {
+  if (cachedDb && cachedDb.serverConfig.isConnected()) {
+    return cachedDb;
+  }
+  const client = await new MongoClient.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  const db = await client.db('noteIt');
+  cachedDb = db;
+  return { db, client };
+}
+
+async function loginUser(data) {
+  let { db, client } = await connectToDatabase()
+  let secretOrKey = process.env.SECRET || 'Dakota123456';
+  try {
+    const { email, password } = validateLogin(JSON.parse(data))
+    let user = await db.collection('users').findOne({ email });
+    if(!user) throw [{ msg: 'Email not found', id: 9 }];
+    let matched = await comparePasswords(password, user.password)
+    if (matched) {
+      let token = jwt.sign({ id: user.id, name: user.name }, secretOrKey, { expiresIn: 31556926 });
+      return token
+    } else {
+      throw [{ msg: 'Password isn\'t correct', id: 10 }];
+    }
+  } catch(error) {
+      return error
+  } finally {
+    await client.close()
+  }
 }
 
 exports.handler = async function(event, context) {
   context.callbackWaitsForEmptyEventLoop = false;
 
-  if (conn == null) {
-    conn = mongoose.createConnection(URI, {
-      bufferCommands: false, 
-      bufferMaxEntries: 0,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true
-    });
-    await conn;
-    conn.model('test-users', UserSchema)
-  }
+  try {   
+    let response = await loginUser(event.body)
+    if(Array.isArray(response)) throw response
 
-  async function login() {
-    let User = conn.model('test-users');
-    let secretOrKey = process.env.SECRET;
-    try {
-      validateLogin(JSON.parse(event?.body));
-      const { email, password } = JSON.parse(event?.body);
-      let user = await User.findOne({ email });
-      if (!user) throw [{ msg: 'Email not found', id: 9 }];
-      let matched = await user.comparePasswords(password);
-      if (matched) {
-        let token = jwt.sign({ id: user.id, name: user.name }, secretOrKey, { expiresIn: 31556926 });
-        return {
-          statusCode: 200,
-          body: JSON.stringify({ success: true, token: `Bearer${token}` })
-        }
-      } else {
-        throw [{ msg: 'Password isn\'t correct', id: 10 }];
-      }
-    } catch (errors) {
-      return {
-        statusCode: 400,
-        body: JSON.stringify(errors)
-      }
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ success: true, token: `Bearer${response}` })
+    }
+  } catch (errors) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify(errors)
     }
   }
-  return login()
-  // return {
-  //   statusCode: 200,
-  //   body: 'Hola'
-  // }
 }
