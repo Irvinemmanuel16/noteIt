bf0f82c904c918e813e096971b7f3411fc58b212
Irvin Meneses <developer@irvinemmanuel.xyz>
2021-04-04 21:12:57 -0500

    Se agrego la funcion registrar usuario

--- functions/users/registerUser.js
+++ functions/users/registerUser.js
@@ -1,35 +1,17 @@
-const mongoose = require('mongoose');
+const { MongoClient } = require('mongodb');
 const bcrypt = require('bcryptjs');
 const Validator = require('validator');
 const isEmpty = require('is-empty');
 
-let conn = null;
 const db_name = encodeURIComponent(process.env.DB_NAME);
 const db_password = encodeURIComponent(process.env.DB_PASSWORD);
 const db_user = process.env.DB_USER;
 
 const URI = `mongodb+srv://${db_user}:${db_password}@cluster0.w5lai.mongodb.net/${db_name}?retryWrites=true&w=majority`;
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
+let cachedDb = null;
 
-}, {
-  timestamps: true
-});
-
-UserSchema.methods.encryptPassword = async password => {
+async function encryptPassword(password) {
   let salt = await bcrypt.genSalt(10);
   let hash = await bcrypt.hash(password, salt);
   return hash;
@@ -50,49 +32,56 @@ function validateRegister({ name, email, password, password2 }) {
   if (Validator.isEmpty(password2)) errors.push({ msg: 'Confirm password field is required', id: 5});
   if (!Validator.isLength(password, { min: 6, max: 30 })) errors.push({ msg: 'Password must be at least 6 characters', id: 6});
   if (!Validator.equals(password, password2)) errors.push({ msg: 'Passwords must match', id: 7});
-
   if (!isEmpty(errors)) throw errors;
+
+  return { name, email, password }
+}
+
+async function connectToDatabase() {
+  if (cachedDb && cachedDb.serverConfig.isConnected()) {
+    return cachedDb;
+  }
+  const client = await new MongoClient.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  const db = await client.db('noteIt');
+  cachedDb = db;
+  return { db, client };
+}
+
+async function registerUser(data) {
+  let { db, client } = await connectToDatabase()
+  try {
+    const { name, email, password } = validateRegister(JSON.parse(data));
+    let user = await db.collection('users').findOne({ email })
+    if (user) throw [{ msg: 'Email already exists', id: 8 }];
+    let encryptedPassword = await encryptPassword(password)
+    await db
+      .collection('users')
+      .insertOne({ name, email, password: encryptedPassword })
+  } catch (err) {
+      return err
+  } finally {
+    await client.close();
+  }
 }
 
 exports.handler = async function(event, context) {
   context.callbackWaitsForEmptyEventLoop = false;
 
-  if (conn == null) {
-    conn = mongoose.createConnection(URI, {
-      bufferCommands: false, 
-      bufferMaxEntries: 0,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      // autoIndex: false
-      useCreateIndex: true
-    });
-    await conn;
-    conn.model('test-users', UserSchema)
-  }
-
-  async function register() {
-    let User = conn.model('test-users');
-    try {
-      validateRegister(JSON.parse(event?.body));
-      const { name, email, password } = JSON.parse(event?.body);
-      let userEmail = await User.findOne({ email });
-      if (userEmail) throw [{ msg: 'Email already exists', id: 8 }];
-      let newUser = new User({ name, email });
-      newUser.password = await newUser.encryptPassword(password);
-      await newUser.save();
-      return {
-        statusCode: 200,
-        body: 'User registered succesfully'
-      }
-    } catch (errors) {
-      console.log(errors)
-      return {
-        statusCode: 400,
-        body: JSON.stringify(errors)
-      }
+  try {   
+    let errors = await registerUser(event.body)
+    if(errors) throw errors
+    return {
+      statusCode: 200,
+      body: 'User registered succesfully'
+    }
+  } catch (errors) {
+    // console.log(errors)
+    return {
+      statusCode: 400,
+      body: JSON.stringify(errors)
     }
   }
-
-  return register()
 }
