b90999a4a223c88d5c88435399c6408b248e297a
Irvin Meneses <developer@irvinemmanuel.xyz>
2021-04-04 20:20:15 -0500

    Se implemento la funcion crear nota

--- functions/notes/createNote.js
+++ functions/notes/createNote.js
@@ -1,58 +1,59 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
+const { MongoClient } = require('mongodb');
 
-let conn = null;
+// let conn = null;
 const db_name = encodeURIComponent(process.env.DB_NAME);
 const db_password = encodeURIComponent(process.env.DB_PASSWORD);
 const db_user = process.env.DB_USER;
 
 const URI = `mongodb+srv://${db_user}:${db_password}@cluster0.w5lai.mongodb.net/${db_name}?retryWrites=true&w=majority`;
 
-let noteSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true
-  },
-  content: [{
-    type: { type: String, required: true },
-    children: [{
-      text: { type: String, required: true },
-      bold: Boolean,
-      underline: Boolean,
-      italic: Boolean
-    }]
-  }],
-  author: {
-    type: String,
-    required: true
+let cachedDb = null;
+
+async function connectToDatabase() {
+  if (cachedDb && cachedDb.serverConfig.isConnected()) {
+    return cachedDb;
   }
-}, {
-  timestamps: true
-})
+  const client = await new MongoClient.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  const db = await client.db('noteIt');
+  cachedDb = db;
+  return { db, client };
+}
+
+async function createNote(data) {
+  let { db, client } = await connectToDatabase()
+  try {
+    let { title, author, content } = JSON.parse(data)
+    const note = await db
+      .collection('notes')
+      .insertOne({ title, author, content })
+    return note;
+  } catch (err) {
+    console.log(err);
+  } finally {
+    await client.close();
+  }
+}
 
 exports.handler = async function(event, context) {
+
   context.callbackWaitsForEmptyEventLoop = false;
-
-  if (conn == null) {
-    conn = mongoose.createConnection(URI, {
-      bufferCommands: false, 
-      bufferMaxEntries: 0,
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: true,
-      autoIndex: false
-    });
-    await conn;
-    conn.model('test', noteSchema)
-  }
   
-  let Note = conn.model('test');
-  let { title, author, content } = JSON.parse(event?.body);
-  let newNote = new Note({ title, author, content })
-  let { _id: id } = await newNote.save()
-  mongoose.connection.close()
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(id)
+  try {
+    const { insertedId } = await createNote(event.body);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(insertedId)
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ msg: err.message }) 
+    };
   }
 };
+
--- functions/notes/getNotes.js
+++ functions/notes/getNotes.js
@@ -6,32 +6,33 @@ const db_name = encodeURIComponent(process.env.DB_NAME);
 const db_password = encodeURIComponent(process.env.DB_PASSWORD);
 const db_user = process.env.DB_USER;
 
-const URI = `mongodb+srv://${db_user}:${db_password}@cluster0.w5lai.mongodb.net/${db_name}?retryWrites=true&w=majority`;
-
-let noteSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true
-  },
-  content: [{
-    type: { type: String, required: true },
-    children: [{
-      text: { type: String, required: true },
-      bold: Boolean,
-      underline: Boolean,
-      italic: Boolean
-    }]
-  }],
-  author: {
-    type: String,
-    required: true
-  }
-}, {
-  timestamps: true
-})
+// const URI = `mongodb+srv://${db_user}:${db_password}@cluster0.w5lai.mongodb.net/${db_name}?retryWrites=true&w=majority`;
+const URI = `mongodb+srv://admin_1:165IyY7radRG1Rd5@cluster0.w5lai.mongodb.net/noteIt?retryWrites=true&w=majority`;
 
 exports.handler = async function(event, context) {
   context.callbackWaitsForEmptyEventLoop = false;
+  
+  let noteSchema = new mongoose.Schema({
+    title: {
+      type: String,
+      required: true
+    },
+    content: [{
+      type: { type: String, required: true },
+      children: [{
+        text: { type: String, required: true },
+        bold: Boolean,
+        underline: Boolean,
+        italic: Boolean
+      }]
+    }],
+    author: {
+      type: String,
+      required: true
+    }
+  }, {
+    timestamps: true
+  })
 
   if (conn == null) {
     conn = mongoose.createConnection(URI, {
@@ -43,12 +44,12 @@ exports.handler = async function(event, context) {
       autoIndex: false
     });
     await conn;
-    conn.model('test', noteSchema)
+    // conn.model('test', noteSchema)
   }
-
-  let Note = conn.model('test');
-  let notes = await Note.find()
-  mongoose.connection.close()
+  // let Note = mongoose.model('test', noteSchema);
+  // let notes = await Note.find()
+  // mongoose.connection.close()
+  console.log(db_password)
   return {
     statusCode: 200,
     body: JSON.stringify(notes)
