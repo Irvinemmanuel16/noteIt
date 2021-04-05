6992190c3f780c0de06a24afd681624513a9a251
Irvin Meneses <developer@irvinemmanuel.xyz>
2021-04-05 11:40:22 -0500

    Resolviendo fallos

--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -19,7 +19,7 @@ export const registerUser = (userData, history) => dispatch => {
 
 export const loginUser = userData => dispatch => {
   axios
-    .post('https://gallant-hypatia-7787f3.netlify.app/.netlify/functions/loginUser', userData)
+    .post('/.netlify/functions/loginUser', userData)
     .then(res => {
       console.log(res)
       const { token } = res.data;
